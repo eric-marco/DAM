082b1ce4566030e49bb0825bc7255173eb129663
Mario Clase <sirof@proton.me>
2026-03-18 09:27:51 +0100

    Conflicto resuelto

--- Entornos/src/GitHub/Calculadora.java
+++ Entornos/src/GitHub/Calculadora.java
@@ -3,9 +3,10 @@ public class Calculadora {
  public double sumar(double a, double b) { return a + b; }
  public double restar(double a, double b) { return a - b; }
  public double multiplicar(double a, double b) { return a * b; }
+ public double dividir(double a, double b) { return a / b; }
  public static void main(String[] args) {
  Calculadora calc = new Calculadora();
- System.out.println("--- CALCULADORA DEL JEFE A ---");
+ System.out.println("--- CALCULADORA DEL EQUIPO A y B  ---");
  System.out.println("Suma: " + calc.sumar(10, 5));
  }
 }
